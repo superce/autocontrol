3ee1b05c5fcf902d2d779c174dfbf7f8ccbdd1da
superce <1594007655@qq.com>
2020-06-05 17:09:00 +0800

    gs

--- src/components/disControl/index.vue
+++ src/components/disControl/index.vue
@@ -1,34 +1,5 @@
 <template>
     <el-dialog :visible.sync="dialogFormControl" title="分配中控">
-        <!-- <el-table :data="controlList" style="width: 100%" height="550" ref='controlTable' stripe @selection-change="handleSelectionChange">
-            <el-table-column type="selection" width="55"></el-table-column>
-            <el-table-column type="expand">
-                <template slot-scope="props">
-                    <el-form label-position="left" inline class="demo-table-expand"> 
-                        <el-form-item label="中控名称:">
-                            <span>{{ props.row.name }}</span>
-                        </el-form-item>
-                        <el-form-item label="ip:">
-                            <span>{{ props.row.ip }}</span>
-                        </el-form-item>
-                        <el-form-item label="队列标识:">
-                            <span>{{ props.row.queue_title }}</span>
-                        </el-form-item>
-                        <el-form-item label="uid:">
-                            <span>{{ props.row.uid }}</span>
-                        </el-form-item>
-                        <el-form-item label="使用人:">
-                            <span>{{ props.row.username }}</span>
-                        </el-form-item>
-                        <el-form-item label="心跳时间:">
-                            <span>{{dateFormat(props.row.lastcore)}}</span>
-                        </el-form-item>
-                    </el-form>
-                </template>
-            </el-table-column>
-            <el-table-column label="中控名称" prop="name"></el-table-column>
-            <el-table-column label="ip" prop="ip"></el-table-column>
-        </el-table> -->
           <div style="text-align: center">
             <el-transfer
             style="text-align: left; display: inline-block;"
@@ -39,18 +10,10 @@
             :render-content="renderFunc"
             :titles="['未分配', '已分配']"
             :button-texts="['取消分配', '分配中控']"
-            
             @change="handleChange"
             :data="data">
-            <!-- @left-check-change='leftChange' -->
-            <!-- <el-button class="transfer-footer" slot="left-footer" size="small">操作</el-button>
-            <el-button class="transfer-footer" slot="right-footer" size="small">操作</el-button> -->
             </el-transfer>
         </div>
-        <!-- <div slot="footer" class="dialog-footer">
-            <el-button @click="dialogFormControl = false">取 消</el-button>
-            <el-button type="primary" @click="confirmSaveControl">确 定</el-button>
-        </div> -->
     </el-dialog>
 </template>
 <script>
@@ -60,25 +23,6 @@ export default {
     props:['rowId','type','taskId'],
     name:'disControl',
     data(){
-        // const generateData = _ => {
-        //     const data = [];
-        //     // for (let i = 1; i <= 15; i++) {
-        //     //     data.push({
-        //     //         key: i,
-        //     //         label: `备选项 ${ i }`,
-        //     //         disabled: i % 4 === 0
-        //     //     });
-        //     // }
-        //     console.log(this.controlList)
-        //     for(let i = 1;i<=this.controlList.length;i++){
-        //         console.log(this.controlList[i])
-        //         data.push({
-        //             key:i,
-        //             label:''
-        //         })
-        //     }
-        //     return data;
-        // };
         return {
             SelectionChange:[], // 选择的列表id
             unIds:[],
@@ -130,20 +74,6 @@ export default {
         },
         // 左右分配按钮
         handleChange(value, direction, movedKeys) {
-            // console.log(value, direction, movedKeys);
-            // let r = []
-            // this.unIds = []
-            // // 过滤已选择的id
-            // r = this.controlList.filter(f =>{
-            //     return this.SelectionChange.indexOf(f.id)==-1
-            // })
-            // r.forEach(r =>{
-            //     this.unIds.push(r.id)
-            // })
-            // let id = []
-            // movedKeys.map(val =>{
-            //     id.push(val.toString())
-            // })
             if(direction === 'left'){ // 取消分配
                 this.confirmSaveControl(movedKeys,2)
                 // console.log(id)
@@ -163,20 +93,10 @@ export default {
         },
         // 确认分配
         confirmSaveControl(id,state){
-            // let r = []
-            // this.unIds = []
-            // // 过滤已选择的id
-            // r = this.controlList.filter(f =>{
-            //     return this.SelectionChange.indexOf(f.id)==-1
-            // })
-            // r.forEach(r =>{
-            //     this.unIds.push(r.id)
-            // })
             let t = {
                 userid:this.userId,
                 ids:id,//this.SelectionChange,
                 type:3,
-                //unids:this.unIds,
                 state:state,
 
             }
@@ -199,8 +119,6 @@ export default {
             }
             apiDistrControl(t).then(res =>{
                 if(res.data.state=='success'){
-                    // this.$message.success('分配成功')
-                    //this.dialogFormControl = false
                 }else{
                     this.$message.error(res.data.msg)
                 }
@@ -218,85 +136,34 @@ export default {
                 page_size: 9999,
             }).then(res => {
                 console.log(res)
-                // this.controlListData(_this.type)
                 let newData = []
-                // let name = []
-                // if(_this.type === 'user'){ // 用户管理
                 res.data.forEach(item => {
                     if(_this.type === 'user'){
                         if(item.userid === _this.rowId || !item.userid){
                             if(item.userid !==0){
-                                // setTimeout(() =>{
-                                //     //this.$refs.controlTable.toggleRowSelection(item);
-                                // },0)
-                                // name.push(item.name)
                                 this.value.push(item.id)
                             }
                             newData.push(item)
-                            // if(item.status!='-1'){
-                            // }
                         }
                     }
                     if(_this.type === 'quene'){
                         // console.log(res.data)
                         if(item.task_queue_id === _this.rowId||!item.task_queue_id){
                             if(item.task_queue_id !==0){
-                                // setTimeout(() =>{
-                                //     //this.$refs.controlTable.toggleRowSelection(item);
-                                // },0)
-                                // name.push(item.name)
                                 this.value.push(item.id)
                             }
-                            //  if(item.status!='-1'){
-                            //      }
                             newData.push(item)
                         }
                     }
                     if(_this.type === 'queneTag'){
                         if(item.queue_tag_id === _this.rowId||!item.task_queue_id){
                             if(item.queue_tag_id !==0){
-                                // setTimeout(() =>{
-                                //     //this.$refs.controlTable.toggleRowSelection(item);
-                                // },0)
-                                // name.push(item.name)
                                 this.value.push(item.id)
                             }
-                            // if(item.status!='-1'){
-                            //     }
                             newData.push(item)
                         }
                     }
                 });
-                // }
-                // if(_this.type === 'quene'){ //队列标识
-                //     console.log(res.data)
-                //     res.data.forEach(item =>{
-                //         if(item.task_queue_id === _this.rowId||!item.task_queue_id){
-                //             if(item.task_queue_id !==0){
-                //                 setTimeout(() =>{
-                //                     this.$refs.controlTable.toggleRowSelection(item);
-                //                 },0)
-                //             }
-                //             //  if(item.status!='-1'){
-                //             //      }
-                //             newData.push(item)
-                //         }
-                //     })
-                // }
-                // if(_this.type === 'queneTag'){
-                //     res.data.forEach(item =>{
-                //         if(item.queue_tag_id === _this.rowId||!item.task_queue_id){
-                //             if(item.queue_tag_id !==0){
-                //                 setTimeout(() =>{
-                //                     this.$refs.controlTable.toggleRowSelection(item);
-                //                 },0)
-                //             }
-                //             // if(item.status!='-1'){
-                //             //     }
-                //             newData.push(item)
-                //         }
-                //     })
-                // }
                 _this.controlList=newData //= newData;
                 _this.dialogFormControl = true
                 _this.generateData()
@@ -305,53 +172,6 @@ export default {
             });
             
         },
-        // controlListData(type){
-        //     let newData = []
-        //         if(_this.type === 'user'){ // 用户管理
-        //             res.data.forEach(item => {
-
-        //                 if(item.userid === _this.rowId || !item.userid){
-        //                     if(item.userid !==0){
-        //                         setTimeout(() =>{
-        //                             this.$refs.controlTable.toggleRowSelection(item);
-        //                         },0)
-        //                     }
-        //                     newData.push(item)
-        //                     // if(item.status!='-1'){
-        //                     // }
-        //                 }
-        //             });
-        //         }
-        //         if(_this.type === 'quene'){ //队列标识
-        //             console.log(res.data)
-        //             res.data.forEach(item =>{
-        //                 if(item.task_queue_id === _this.rowId||!item.task_queue_id){
-        //                     if(item.task_queue_id !==0){
-        //                         setTimeout(() =>{
-        //                             this.$refs.controlTable.toggleRowSelection(item);
-        //                         },0)
-        //                     }
-        //                     //  if(item.status!='-1'){
-        //                     //      }
-        //                     newData.push(item)
-        //                 }
-        //             })
-        //         }
-        //         if(_this.type === 'queneTag'){
-        //             res.data.forEach(item =>{
-        //                 if(item.queue_tag_id === _this.rowId||!item.task_queue_id){
-        //                     if(item.queue_tag_id !==0){
-        //                         setTimeout(() =>{
-        //                             this.$refs.controlTable.toggleRowSelection(item);
-        //                         },0)
-        //                     }
-        //                     // if(item.status!='-1'){
-        //                     //     }
-        //                     newData.push(item)
-        //                 }
-        //             })
-        //         }
-        // },
         // 格式化时间
         dateFormat(date) {
             if(date){
